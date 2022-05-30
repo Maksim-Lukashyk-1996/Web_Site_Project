5b5584ab0c6fc2175fb92219d3db20bc328a288f
Maksim <stik9935@gmail.com>
2022-05-30 17:15:12 +0300

    Web_Site_Project

--- Web_Site_App/urls.py
+++ Web_Site_App/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Web_Site_App.urls'))
+    path('', views.index),
+    path('Hello/', views.Hello),
+    path('About_us/', views.About_us)
 ]
--- Web_Site_App/views.py
+++ Web_Site_App/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'Web_Site/index.html')
+
+def Hello(request):
+    return HttpResponse('<h1>Hello, have a nice day! :)</h1>')
+
+def About_us(request):
+    return render(request, 'Web_Site/about.html')
