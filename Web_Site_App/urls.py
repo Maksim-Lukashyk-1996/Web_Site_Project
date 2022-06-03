1e70abb1ff2a519334d348229a69325a24781211
Maksim <stik9935@gmail.com>
2022-06-03 13:28:52 +0300

    Web_Site_Project

--- Web_Site_App/urls.py
+++ Web_Site_App/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('About_us/', views.About_us, name='about'),
     path('Create/', views.Create, name='create'),
-    path('Delete/', views.Delete, name='delete')
+    path('Delete/', views.DeleteView.as_view(), name='delete')
 ]
--- Web_Site_App/views.py
+++ Web_Site_App/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
-from .forms import TaskForm
+from django.views.generic import DeleteView
 
 from .forms import TaskForm
 
@@ -13,16 +13,23 @@ def About_us(request):
     return render(request, 'Web_Site/about.html')
 
 def Create(request):
+    error = ''
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('home')
+        else:
+            error = 'Неверная форма'
 
     form = TaskForm()
     context = {
-        'form': form
+        'form': form,
+        'error': error
     }
     return render(request, 'Web_Site/create.html', context)
 
-def Delete(request):
-    return render(request, 'Web_Site/delete.html')
+class DeleteView(DeleteView):
+    model = Task
+    success_url = 'home'
+    template_name = 'Web_Site/delete.html'
