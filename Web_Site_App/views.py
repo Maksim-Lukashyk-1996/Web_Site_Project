db2ddc9f369a80e039ef25f624f424a66561f7a8
Maksim <stik9935@gmail.com>
2022-06-03 11:40:19 +0300

    Web_Site_Project

--- Web_Site_App/views.py
+++ Web_Site_App/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
+
+from .forms import TaskForm
 
 def index(request):
+    if request.method == 'POST':
+        error = ''
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            error = 'Неверная форма'
     tasks = Task.objects.order_by('-id')
     return render(request, 'Web_Site/index.html', {'title': 'Главная страница', 'tasks': tasks})
 
@@ -10,7 +20,11 @@ def About_us(request):
     return render(request, 'Web_Site/about.html')
 
 def Create(request):
-    return render(request, 'Web_Site/create.html')
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'Web_Site/create.html', context)
 
 def Delete(request):
     return render(request, 'Web_Site/delete.html')
